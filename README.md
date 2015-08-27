950976ba620407913d2d7216caf892d6042cad3f
Forrest L Norvell <forrest@npmjs.com>
2015-08-26 22:09:01 -0700

    src: revert previous commit

---
+++ README.md
@@ -0,0 +1,39 @@
+If [`deeper`](http://npm.im/deeper) and [`deepest`](http://npm.im/deepest) are
+`assert.deepEqual()`'s strict East Coast siblings with engineering backgrounds,
+`only-shallow` is their laid-back California cousin.  `only-shallow` is a
+library for structurally comparing JavaScript objects. It supports recursive /
+cyclical data structures, is written to avoid try / catch / throw (for speed),
+and has no dependencies. It's not particularly strict about matching types.
+It's more of a duck squeezer.
+
+It has some optimizations but stresses correctness over raw speed. Unlike
+`deeper` and `deepest`, it has no native dependencies, so you can use it, like,
+wherever.
+
+The core algorithm is based on those used by Node's assertion library and the
+implementation of cycle detection in
+[isEqual](http://underscorejs.org/#isEqual) in
+[Underscore.js](http://underscorejs.org/).
+
+I like to think the documentation is pretty OK.
+
+`only-shallow` has this name because [I'm
+old](https://www.youtube.com/watch?v=oiomcuNlVjk).
+
+## installation
+
+```
+npm install only-shallow
+```
+
+## usage
+
+```javascript
+var deepEqual = require('deeper')
+
+if (!deepEqual(obj1, obj2)) console.log("yay! diversity!");
+```
+
+## license
+
+ISC. Go nuts.
---
+++ test/deep.js
@@ -0,0 +1,58 @@
+var tap = require('tap')
+var test = tap.test
+var same = require('../')
+
+test("shallower shouldn't care about key order and types", function (t) {
+  t.ok(same({ a: 1, b: 2 }, { b: 2, a: '1' }))
+  t.end()
+})
+
+test('shallower find same arrays', function (t) {
+  t.ok(same([1, 2, 3], [1, 2, 3]))
+  t.end()
+})
+
+test('shallower find different arrays', function (t) {
+  t.notOk(same([1, 2, 3], [1, 2, 3, 4]))
+  t.notOk(same([1, 2, 3], [1, 2, 4]))
+  t.end()
+})
+
+test("shallower shouldn't care about key order recursively and types", function (t) {
+  t.ok(same(
+    { x: { a: 1, b: 2 }, y: { c: 3, d: 4 } },
+    { y: { d: 4, c: 3 }, x: { b: '2', a: '1' } }
+  ))
+  t.end()
+})
+
+test("shallower shouldn't care about key order (but still might) and types", function (t) {
+  t.ok(same(
+    [
+      { foo: { z: 100, y: 200, x: 300 } },
+      'bar',
+      11,
+      { baz: { d: 4, a: 1, b: 2, c: 3 } }
+    ],
+    [
+      { foo: { z: 100, y: 200, x: 300 } },
+      'bar',
+      11,
+      { baz: { a: '1', b: '2', c: '3', d: '4' } }
+    ]
+  ))
+  t.end()
+})
+
+test("shallower shouldn't blow up on circular data structures", function (t) {
+  var x1 = { z: 4 }
+  var y1 = { x: x1 }
+  x1.y = y1
+
+  var x2 = { z: 4 }
+  var y2 = { x: x2 }
+  x2.y = y2
+
+  t.ok(same(x1, x2))
+  t.end()
+})
